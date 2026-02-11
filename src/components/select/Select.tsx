3b6656332e2391fbdd18accb9dd6c9aad926da4a
Ferdinand GOOR <ferdinand.goor@gmail.com>
2026-02-11 11:28:12 +0100

    FIX resize

--- src/components/select/Select.tsx
+++ src/components/select/Select.tsx
@@ -29,13 +29,6 @@ const Select = () => {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [selectedId]);
 
-  useEffect(() => {
-    const onResize = () => measure();
-    window.addEventListener("resize", onResize);
-    return () => window.removeEventListener("resize", onResize);
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
-
   return (
     <div
       className="select-tabs"
