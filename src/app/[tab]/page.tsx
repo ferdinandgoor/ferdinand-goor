888c0df454554c8aa5e3cb159516d3a60bb65f06
Ferdinand GOOR <ferdinand.goor@gmail.com>
2026-02-11 09:05:05 +0100

    fix urls

--- src/app/[tab]/page.tsx
+++ src/app/[tab]/page.tsx
@@ -2,6 +2,14 @@
 
 import Home from "../page";
 
+export const dynamicParams = false;
+
+export const generateStaticParams = () => [
+  { tab: "video" },
+  { tab: "music" },
+  { tab: "youtube" },
+];
+
 const TabPage = () => <Home />;
 
 export default TabPage;
